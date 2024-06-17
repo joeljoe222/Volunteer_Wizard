b1facfbcda6a6af8c1258a6393a9057d207e6be0
Joel Thomas <joelkthomas99@gmail.com>
2024-06-17 13:22:30 -0500

    NavBar and few dummy Pages

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello M";
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__': app.run(host='0.0.0.0', debug=True)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__': app.run(host='0.0.0.0', debug=True) # starts server
