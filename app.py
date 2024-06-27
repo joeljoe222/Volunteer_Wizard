f96800dc12ccff9412600011ec8b8d73a72fd8c7
Jay Mejia <jayeatsghost@gmail.com>
2024-06-27 12:55:40 -0500

    This includes Event Management Page and Notification System

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello M";
+def index():
+    return render_template("index.html")
+
+@app.route("/eventManager")
+def eventManager():
+    return render_template("eventManager.html");
+
+@app.route("/notificationSystem")
+def notificationManager():
+    return render_template("notificationSystem.html");
 
 if __name__ == '__main__': app.run(host='0.0.0.0', debug=True)
