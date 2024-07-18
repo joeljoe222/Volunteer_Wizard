4a8295d065cbd344d85fee7e03577395e9fd99e9
Jay Mejia <jayeatsghost@gmail.com>
2024-07-18 00:09:15 -0500

    New Create Event Page, Mange Event Page prefills with the data of the event being managed

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from forms import NotificationForm, EventForm
+from forms import NotificationForm, EventCreateForm, EventManageForm
+import datetime
 
+#TODO LIST :
+#stallmake event seen from events page and update as edits are made
+#do the same with notification page
+#change all varibles and functions to snake_case
+#change html urls to be event-page
+#comment all work
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
+
+
+#Example Event
+event_data = {
+    'eventName':'Example Event',
+    'eventDesc':'Here is where an Admin would write a description for the Event',
+    'eventDate':datetime.date(2024, 7, 24),
+    'urgency':'1',
+    'eventAddress':'404 Street Name',
+    'country':'USA',
+    'state':'TX',
+    'zipcode':'11220',
+    'requiredSkills':['a','c']
+}
+
+#Example Notification
+notification_data = {
+    'notifName':'Notification Title',
+    'notifDesc':'This is where the main notification information will be'
+}
 
 # just sample until DB
 states = [
@@ -66,23 +93,18 @@ def notificationManager():
 def notification():
     form = NotificationForm()
     if form.validate_on_submit():
-        notifName = form.notifName.data
-        notifDesc = form.notifDesc.data
+        notification_data['notifName'] = form.notifName.data
+        notification_data['notifDesc'] = form.notifDesc.data
         
         flash(f'Notification Sent! : {form.notifName.data} <br> {form.notifDesc.data}','success')
         
         return redirect(url_for('notification'))
-    else:
-        print('notif failed validation')
-        print(form.errors)
     return render_template('notification.html', form=form)
 
-@app.route("/eventManager", methods=['GET','POST'])
-def eventManager():
-    form = EventForm()
+@app.route("/eventCreator", methods=['GET','POST'])
+def eventCreator():
+    form = EventCreateForm()
     if form.validate_on_submit():
-        print('Event Form Validated')
-
         eventName = form.eventName.data
         eventDesc = form.eventDesc.data
         eventDate = form.eventDate.data
@@ -93,17 +115,45 @@ def eventManager():
         zipcode = form.zipcode.data
         requiredSkills = form.requiredSkills.data
 
-        flash(f'The Event : {form.eventName.data} has been successfully updated','success')
-        
-        print('Event Flash')
+        flash(f'The Event : {form.eventName.data} has been successfully created','success')
 
-        form = EventForm()
+        form = EventCreateForm()
         
+        return redirect(url_for('eventCreator'))
+    return render_template("eventCreator.html", form=form)
+
+@app.route("/eventManager", methods=['GET','POST'])
+def eventManager():
+    form = EventManageForm(obj=event_data)
+    #form.eventName.data = event_data['eventName']
+    if request.method == 'GET':
+        form.eventName.data = event_data['eventName']
+        form.eventDesc.data = event_data['eventDesc']
+        form.eventDate.data = event_data['eventDate']
+        form.urgency.data = event_data['urgency']
+        form.eventAddress.data = event_data['eventAddress']
+        form.country.data = event_data['country']
+        form.state.data = event_data['state']
+        form.zipcode.data = event_data['zipcode']
+        form.requiredSkills.data = event_data['requiredSkills']
+    if form.validate_on_submit():
+        #form.populate_obj(event_data)
+        event_data['eventName'] = form.eventName.data
+        event_data['eventDesc'] = form.eventDesc.data
+        event_data['eventDate'] = form.eventDate.data
+        event_data['urgency'] = form.urgency.data
+        event_data['eventAddress'] = form.eventAddress.data
+        event_data['country'] = form.country.data
+        event_data['state'] = form.state.data
+        event_data['zipcode'] = form.zipcode.data
+        event_data['requiredSkills'] = form.requiredSkills.data
+
+        flash(f'The Event : {form.eventName.data} has been successfully updated','success')
         return redirect(url_for('eventManager'))
-    else:
-        print('Event did NOT validate')
-        print(form.errors)
-    return render_template("eventManager.html", form=form)
+    print("Event Data: ", event_data)
+    print("Form Data: ", form.data)
+
+    return render_template("eventManager.html", form=form, event=event_data)
 
 @app.route("/event")
 def event():
--- forms.py
+++ forms.py
@@ -2,34 +2,63 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, DateField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, Length
 
+urgencyLevel = [
+    ('','Select Urgency'),
+    ('0','Level 0'),
+    ('1','Level 1'),
+    ('2','Level 2'),
+    ('3','Level 3')
+]
+
+skills = [
+    ('a','Skill 1'),
+    ('b','Skill 2'),
+    ('c','Skill 3')
+]
+
+country = [
+    ('','Select Country'),
+    ('USA','United States'),
+    ('0','Other')
+]
+
+state = [
+    ('','Select state'),
+    ('TX','Texas'),
+    ('FL','Florida'),
+    ('NY','New York'),
+    ('CA','California')
+]
+
+#Naming SubjectAction()
+
 class NotificationForm(FlaskForm):
     notifName = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
     notifDesc = TextAreaField('Notification', validators=[DataRequired(), Length(max=300)])
     submit = SubmitField('Push Notification')
 
-class EventForm(FlaskForm):
+
+class EventCreateForm(FlaskForm):
     eventName = StringField('Name of Event', validators=[DataRequired(), Length(min=2, max=100)])
     eventDesc = TextAreaField('Event Description', validators=[DataRequired()])
     eventDate = DateField('Event Date', validators=[DataRequired()])
-    urgency = SelectField('Urgency', validators=[DataRequired()], 
-        choices=[
-            ('0','Level 0'),
-            ('1','Level 1'),
-            ('2','Level 2'),
-            ('3','Level 3')
-        ])
+    urgency = SelectField('Urgency', validators=[DataRequired()], choices=urgencyLevel)
     eventAddress = StringField('Address', validators=[DataRequired()])
-    country = SelectField('Country', validators=[DataRequired()], choices= [('USA','United States')])
-    state = SelectField('State', validators=[DataRequired()],
-        choices=[
-            ('TX','Texas'),
-            ('FL','Florida')
-        ])
+    country = SelectField('Country', validators=[DataRequired()], choices=country)
+    state = SelectField('State', validators=[DataRequired()], choices=state)
     zipcode = StringField('Zipcode', validators=[DataRequired(), Length(min=5, max=10)])
-    requiredSkills = SelectMultipleField('Required Skills', validators=[DataRequired()], 
-        choices=[
-            ('skill1','Skill 1'),
-            ('skill2','Skill 2'),
-            ('skill3','Skill 3')
-        ])
+    requiredSkills = SelectMultipleField('Required Skills', validators=[DataRequired()], choices=skills)
+    submit = SubmitField('Upload Event')
+
+
+class EventManageForm(FlaskForm):
+    eventName = StringField('Name of Event', validators=[DataRequired(), Length(min=2, max=100)])
+    eventDesc = TextAreaField('Event Description', validators=[DataRequired()])
+    eventDate = DateField('Event Date', validators=[DataRequired()])
+    urgency = SelectField('Urgency', validators=[DataRequired()], choices=urgencyLevel)
+    eventAddress = StringField('Address', validators=[DataRequired()])
+    country = SelectField('Country', validators=[DataRequired()], choices=country)
+    state = SelectField('State', validators=[DataRequired()], choices=state)
+    zipcode = StringField('Zipcode', validators=[DataRequired(), Length(min=5, max=10)])
+    requiredSkills = SelectMultipleField('Required Skills', validators=[DataRequired()], choices=skills)
     submit = SubmitField('Update Event')
